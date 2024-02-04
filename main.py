b4c9a288e7fc5ac9f18946d8588ad02f09c39cb9
juan-tamarit <juan.tamarit@outlook.es>
2024-02-04 11:30:59 +0100

    Modificación del turno para la toma de decisiones Se han creado las funciones cura(x) y enviar_ayuda(x,y)     -cura(x) un país que se pasa por parámetro "x" ejecuta el metodo Pais.Heal(value) para recuperar vida, value es una tirada de Dados de D20     -enviar_ayuda(x,y) un país que se pasa por parametro "x" ejecuta la acción ayuda(self,other,value) sobre otro país que se envía por parametro, cediendole una tirada de dado D10 de su propia extensión Para elegir entre estas dos acciones y contiendad(x,y) se ha creado un dado elec que tiene tantas caras como métodos hay en País, al inicio del turno de Pais se tira elec dando las siguientes resultados:     -elec=1 -> ataque a otro páis     -elec=2 -> el país aumenta su extensión     -elec=3 -> el país envía ayuda a otro país

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def contienda(x,y):
     dado=Dado(10)
     x.atac(y,dado.rol())
     y.atac(x,dado.rol())
+def cura(x):
+    dado=Dado(20)
+    x.heal(dado.rol())
+def enviar_ayuda(x,y):
+    dado=Dado(10)
+    x.ayuda(y,dado.rol())
 def mostrar(CPais):
     for pais in CPais.get_paises():
         print(pais.get_ext())
@@ -24,8 +30,15 @@ def selec_objetivo(CPais,x):
 #loop jugable
 prueba=CPais()
 prueba.crear_paises(10)
+elec=Dado(3)
 while (len(prueba.get_paises())>1):
     for pais in prueba.get_paises():
-        contienda(pais,selec_objetivo(prueba,pais))
+        elec_aux=elec.rol()
+        if elec_aux==1:
+            contienda(pais,selec_objetivo(prueba,pais))
+        elif elec_aux==2:
+            cura(pais)
+        elif elec_aux==3:
+            enviar_ayuda(pais,selec_objetivo(prueba,pais))
         limpia(prueba)
     mostrar(prueba)
