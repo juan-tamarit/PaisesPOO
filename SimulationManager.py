f446a642808dc88f9a36aa979476657c324a1eb2
juan-tamarit <juan.tamarit@outlook.es>
2024-02-17 11:59:35 +0100

    Issue #14 Crear clase SimulationManager

---
+++ SimulationManager.py
@@ -0,0 +1,9 @@
+class SimulationManager:
+    def __init__(self,CPais):
+        self.CPais=CPais
+    def guardar():
+        #debe implementarse
+        pass
+    def cargar():
+        #debe implementarse
+        pass
