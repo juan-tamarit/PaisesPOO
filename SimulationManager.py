fd921113b01db7211db733a8444627f556632407
juan-tamarit <juan.tamarit@outlook.es>
2024-02-18 19:12:36 +0100

    Issue #14 Se crea la función cargar

--- SimulationManager.py
+++ SimulationManager.py
@@ -1,3 +1,4 @@
+from Pais import Pais
 class SimulationManager:
     def __init__(self,CPais):
         self.CPais=CPais
@@ -6,5 +7,12 @@ class SimulationManager:
             info_paises="\n".join([f"{pais.get_id()},{pais.get_ext()}" for pais in self.CPais.get_paises()])
             f.write(info_paises)
     def cargar(self):
-        #debe implementarse
-        pass
+        paises=[]
+        with open(".\guardados\guardado.txt","r") as f:
+            for line in f.readlines():
+                info=line.split(",")
+                pais=Pais(int(info[0]))
+                pais.set_ext(int(info[1]))
+                paises.append(pais)
+        self.CPais.set_paises(paises)
+                
--- main.py
+++ main.py
@@ -8,12 +8,11 @@ from Dado import Dado
 from SimulationManager import SimulationManager
 #loop jugable
 prueba=CPais()
-prueba.crear_paises(3)
 root=tk.Tk()
 visor=Visor(root,prueba)
 elec=Dado(3)
 sim= SimulationManager(prueba)
 # Schedule the first game round after a delay and start the GUI main loop
-sim.guardar()
+sim.cargar()
 root.after(1000,lambda: prueba.ronda(elec,root))
 root.mainloop()
