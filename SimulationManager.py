71cb02fc839384814a1b435331a3f7c8bb663d57
juan-tamarit <juan.tamarit@outlook.es>
2024-02-18 19:20:15 +0100

    Issue #14

--- SimulationManager.py
+++ SimulationManager.py
@@ -1,12 +1,45 @@
+#clases del proyecto
 from Pais import Pais
+#clase
 class SimulationManager:
+    """
+    Manages the simulation state, providing methods for saving and loading country information.
+
+    Attributes:
+    - CPais (CPais): The CPais object representing the collection of countries.
+
+    Methods:
+    - __init__(CPais): Initializes a SimulationManager instance with a CPais object.
+    - guardar(): Saves the simulation state to a text file.
+    - cargar(): Loads the simulation state from a text file.
+    """
     def __init__(self,CPais):
+        """
+        Initializes a SimulationManager instance.
+
+        Parameters:
+        - CPais (CPais): The CPais object representing the collection of countries.
+        """
         self.CPais=CPais
     def guardar(self):
+        """
+        Saves the simulation state to a text file.
+
+        This method creates a text file ("guardado.txt") in the "./guardados/" directory
+        and writes the information of each country (ID and external state) in the CPais object
+        to the file in the format "ID,EXT", with each country on a new line.
+        """
         with open (".\guardados\guardado.txt","w") as f:
             info_paises="\n".join([f"{pais.get_id()},{pais.get_ext()}" for pais in self.CPais.get_paises()])
             f.write(info_paises)
     def cargar(self):
+        """
+        Loads the simulation state from a text file.
+
+        This method reads the "guardado.txt" file from the "./guardados/" directory,
+        extracts information for each country (ID and external state), creates Pais objects,
+        and sets the CPais object's list of countries to the loaded data.
+        """
         paises=[]
         with open(".\guardados\guardado.txt","r") as f:
             for line in f.readlines():
@@ -15,4 +48,3 @@ class SimulationManager:
                 pais.set_ext(int(info[1]))
                 paises.append(pais)
         self.CPais.set_paises(paises)
-                
